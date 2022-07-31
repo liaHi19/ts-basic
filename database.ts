bd62cbff76d723b4001b7cb9046afa0a7f8f604d
liaHi19 <gn.natalia19@gmail.com>
2022-07-31 10:58:35 +0300

    [#12] Add database.ts: classes

---
+++ database.ts
@@ -0,0 +1,40 @@
+interface Database {
+  get(id: string): string;
+  set(id: string, value: string): void;
+}
+
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void;
+}
+
+class InMemoryDatabase implements Database {
+  protected db: Record<string, string> = {};
+  get(id: string): string {
+    return this.db[id];
+  }
+  set(id: string, value: string): void {
+    this.db[id] = value;
+  }
+}
+
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState);
+  }
+}
+
+const myDB = new InMemoryDatabase();
+myDB.set("foo", "bar");
+console.log(myDB.get("foo"));
+
+const myDB1 = new PersistentMemoryDB();
+myDB1.set("foo", "bar-bar");
+const saved = myDB1.saveToString();
+
+const myDB2 = new PersistentMemoryDB();
+myDB2.restoreFromString(saved);
+console.log(myDB2.get("foo"));
